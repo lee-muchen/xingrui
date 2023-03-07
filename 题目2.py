dd6e6cbaf0ee84bc0779306289eda9eb7869caaf
muchen <1349454896@qq.com>
2023-03-07 12:39:12 +0800

    星睿融创测试题及说明

--- 题目2.py
+++ 题目2.py
@@ -15,21 +15,27 @@ LU1299722030
 LU0329630999
 LU0329630130
 """
-
+# 定义列表
 text_dict = {}
+# 切割去空格
 text_list = long_text.strip().split('\n')
+# name lei 添加到字典中
 text_dict['name'] = text_list[0]
 text_dict['lei'] = text_list[1]
+# 删除已添加的元素
 del text_list[:2]
 text_dict['sub_fund'] = []
 
 isin_list = []
 sub_fund_dict = {}
+# 循环处理sub_fund内容
 for text in text_list:
-    print(text)
+    # 判断是否有序号
     if text.find('.') > 0:  # 是
+        # 提交上一轮sub——fund信息
         if sub_fund_dict:
             text_dict['sub_fund'].append(sub_fund_dict)
+        # 初始化新sub_fund元素
         sub_fund_dict['title'] = text.split('.')[1].strip(' ')
         sub_fund_dict['isin'] = []
     else:
--- 题目一.py
+++ 题目一.py
@@ -1,18 +1,14 @@
 from selenium import webdriver
 import csv
-header = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.129 Safari/537.36',
-}
+# 打开Chrome浏览器
 driver = webdriver.Chrome("F:/chromedriver.exe")
+# 浏览网址
 driver.get('https://www.chinabond.com.cn/cb/cn/xxpl/ywgg/tgywgg/20230129/161991420.shtml')
-# //*[@id="hiddenContent"]/table/tbody/tr[91]
-# //*[@id="hiddenContent"]/table/tbody/tr[10]/td[6]
-# //*[@id="hiddenContent"]/table/tbody/tr[9]/td[1]
-# test = driver.find_element_by_xpath('//*[@id="hiddenContent"]/table/tbody/tr[9]').text
-# print(test)
 
+# 根据xpath提示表格开始行进行遍历
 for row in range(9, 92):
     row_list = driver.find_element_by_xpath('//*[@id="hiddenContent"]/table/tbody/tr[{}]'.format(str(row))).text.split()
     with open('题目1.csv', 'a+', encoding='utf-8', newline='') as f:
         writer = csv.writer(f)
+        # 提取前六列
         writer.writerow(row_list[:6])
